ea405f2f4d946ac428bd05e0ff3f24c9fcd2a46d
tonyly98 <tony.ly619@gmail.com>
2024-09-19 03:32:54 -0400

    moving data conversion to module

--- src/reddit_scrapper/models/data_conversion/data_conversion.py
+++ src/reddit_scrapper/models/data_conversion/data_conversion.py
@@ -1,2 +1,19 @@
 import json
+import csv
 
+
+def export_json(submission_list: list):
+    jsonInfo = json.dumps(submission_list, indent=4, ensure_ascii=False)
+
+    # writing to file
+    with open("test.json", "w",  newline='', encoding='utf-8') as json_file:
+        json_file.write(jsonInfo)
+        print("JSON Saved!")
+
+def export_csv(submission_list: list):
+    with open("test.csv", "w", newline='', encoding='utf-8') as csv_file:
+        fieldnames = submission_list[0].keys()
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in submission_list:
+            writer.writerow(row)
