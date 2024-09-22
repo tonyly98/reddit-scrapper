236a8fbc45dde4134f6a200fee9fddc638bbe54a
tonyly98 <tony.ly619@gmail.com>
2024-09-22 02:52:36 -0400

    setup for command line arguments

--- src/reddit_scrapper/__main__.py
+++ src/reddit_scrapper/__main__.py
@@ -1,24 +1,59 @@
 import praw
-import json
-import csv
 from models.data_conversion import export_json, export_csv
+import argparse
+from datetime import datetime, timezone
 
+
+parser = argparse.ArgumentParser(
+    description="Provide databack as json or sql"
+)
+
+# Client ID from reddit app
+parser.add_argument(
+    "-id", "--client_id", metavar="client_id",
+    required=True, help= "The client ID of your app"
+)
+
+# Client secret from reddit app
+parser.add_argument(
+    "-secret", "--client_secret", metavar="client_secret",
+    required=True, help= "The client ID of your app"
+)
+
+# Agent name for reddit 
+parser.add_argument(
+    "-agent", "--user_agent", metavar="user_agent",
+    required=True, help= "The client ID of your app"
+)
+
+# Format of exporting
+parser.add_argument(
+    "-data", "--datatype", metavar="datatype",
+    required=False, default="json",help= "Return as CSV or json"
+)
+
+
+args = parser.parse_args()
+print(args.datatype)
+#    client_id="dX5FicqjbH7MM2nwlHnL3Q",
+#    client_secret="lgzyJMsDBpQDsnHeW0Z-FNiEWQA5Cg",
+#    user_agent="cute-subreddit-scrapper"
 reddit = praw.Reddit(
-    client_id="your id heree",
-    client_secret="your secret key here",
-    user_agent="cute-subreddit-scrapper"
+    client_id=args.client_id,
+    client_secret=args.client_secret,
+    user_agent=args.user_agent
 )
 
 # Setting up for gathering info from post
 submission_list = []
-for submission in reddit.subreddit("aww").top(time_filter="week", limit=10):
+for submission in reddit.subreddit("all").top(time_filter="week", limit=10):
     temp_dict = {
-        "id": submission.id,
         "subreddit": submission.subreddit.display_name,
-        "title": submission.title,
+        "id": submission.id,
+        "date": datetime.fromtimestamp(submission.created_utc, timezone.utc).strftime('%Y-%m-%d %H:%M:%S'),
         "author": submission.author.name,
+        "title": submission.title,
         "score": submission.score,
-        "date": submission.created_utc,
         "url": submission.url
     }
     submission_list.append(temp_dict)
--- src/reddit_scrapper/models/command_line/command_line.py
+++ src/reddit_scrapper/models/command_line/command_line.py
@@ -1,12 +1,58 @@
 import argparse
 
+class CommandLine():
+    
 parser = argparse.ArgumentParser(
     description="Provide databack as json or sql"
 )
 
+# Client ID from reddit app
+parser.add_argument(
+    "-id", "--client_id", metavar="client_id",
+    required=True, help= "The client ID of your app"
+)
+
+# Client secret from reddit app
+parser.add_argument(
+    "-secret", "--client_secret", metavar="client_secret",
+    required=True, help= "The client ID of your app"
+)
+
+# Agent name for reddit 
+parser.add_argument(
+    "-agent", "--user_agent", metavar="user_agent",
+    required=True, help= "The client ID of your app"
+)
+
+# Subbreddit to scrape from
+parser.add_argument(
+    "-sub", "--subreddit", metavar="subreddit",
+    required=False, default="all",help= "Subreddit to scrape from"
+)
+
+# Sort by order 
+parser.add_argument(
+    "-sort", "--sort_by", metavar="sort_by",
+    required=False, default="hot",help= "Sort by order of subreddit (hot, new, top, rising, controversial)"
+)
+
+# Time frame (only need for top and controversial sort oders)
+parser.add_argument(
+    "-frame", "--time_frame", metavar="time_frame",
+    required=False, default="month",
+    help= 'Time frame, only need for top and controversial sort oders ("all", "day", "hour", "month", "week", or "year")'
+)
+
+# Number of post
+parser.add_argument(
+    "-limit", "--limit", metavar="limit",
+    required=False, default=100,help= "The number of posts returns, the default is 100, anything over 100 could be unstable"
+)
+
+# Format of exporting
 parser.add_argument(
     "-data", "--datatype", metavar="datatype",
-    required=True, help="Return as DB or json"
+    required=False, default="json",help= "Return as CSV or json"
 )
 
 args = parser.parse_args()
