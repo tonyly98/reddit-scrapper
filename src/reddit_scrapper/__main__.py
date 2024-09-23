46f26bec3abae9df8ba9095ffa5d550198ed9168
tonyly98 <tony.ly619@gmail.com>
2024-09-23 07:47:58 -0400

    formatting for runnable script

--- src/reddit_scrapper/__main__.py
+++ src/reddit_scrapper/__main__.py
@@ -3,34 +3,35 @@ from models.data_conversion import export_json, export_csv
 from models.command_line import parser, subreddits_parser
 from datetime import datetime, timezone
 
-args = parser()
-print(type(args.limit))
-print(args.limit)
+def main():
+    args = parser()
 
-reddit = praw.Reddit(
-    client_id=args.client_id,
-    client_secret=args.client_secret,
-    user_agent=args.user_agent
-)
-print(type(reddit))
+    reddit = praw.Reddit(
+        client_id=args.client_id,
+        client_secret=args.client_secret,
+        user_agent=args.user_agent
+    )
 
-# Setting up for gathering info from post
-submission_list = []
-subreddits = subreddits_parser(args, reddit)
+    # Setting up for gathering info from post
+    submission_list = []
+    subreddits = subreddits_parser(args, reddit)
 
-for submission in subreddits:
-    temp_dict = {
-        "subreddit": submission.subreddit.display_name,
-        "id": submission.id,
-        "date": datetime.fromtimestamp(submission.created_utc, timezone.utc).strftime('%Y-%m-%d %H:%M:%S'),
-        "author": submission.author.name,
-        "title": submission.title,
-        "score": submission.score,
-        "url": submission.url
-    }
-    submission_list.append(temp_dict)
+    for submission in subreddits:
+        temp_dict = {
+            "subreddit": submission.subreddit.display_name,
+            "id": submission.id,
+            "date": datetime.fromtimestamp(submission.created_utc, timezone.utc).strftime('%Y-%m-%d %H:%M:%S'),
+            "author": submission.author.name,
+            "title": submission.title,
+            "score": submission.score,
+            "url": submission.url
+        }
+        submission_list.append(temp_dict)
 
-if args.datatype == "json":    
-    export_json(submission_list)
-else:
-    export_csv(submission_list)
+    if args.datatype == "json":    
+        export_json(submission_list)
+    else:
+        export_csv(submission_list)
+
+if __name__ == '__main__':
+    main()
--- src/reddit_scrapper/models/data_conversion/data_conversion.py
+++ src/reddit_scrapper/models/data_conversion/data_conversion.py
@@ -3,11 +3,9 @@ import csv
 
 def export_json(submission_list: list):
     jsonInfo = json.dumps(submission_list, indent=4, ensure_ascii=False)
-
-    # writing to file
     with open("test.json", "w",  newline='', encoding='utf-8') as json_file:
         json_file.write(jsonInfo)
-        print("JSON Saved!")
+    print("Data saved as json!")
 
 def export_csv(submission_list: list):
     with open("test.csv", "w", newline='', encoding='utf-8') as csv_file:
@@ -16,3 +14,4 @@ def export_csv(submission_list: list):
         writer.writeheader()
         for row in submission_list:
             writer.writerow(row)
+    print("Data saved as csv!")
