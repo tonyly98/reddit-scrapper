2b9f7b55f2e4a8f6a7996d598ecf359b1c1f10f1
tonyly98 <tony.ly619@gmail.com>
2024-09-23 03:10:01 -0400

    parsing of args

--- src/reddit_scrapper/__main__.py
+++ src/reddit_scrapper/__main__.py
@@ -1,52 +1,24 @@
 import praw
 from models.data_conversion import export_json, export_csv
-import argparse
+from models.command_line import parser, subreddits_parser
 from datetime import datetime, timezone
 
+args = parser()
+print(type(args.limit))
+print(args.limit)
 
-parser = argparse.ArgumentParser(
-    description="Provide databack as json or sql"
-)
-
-# Client ID from reddit app
-parser.add_argument(
-    "-id", "--client_id", metavar="client_id",
-    required=True, help= "The client ID of your app"
-)
-
-# Client secret from reddit app
-parser.add_argument(
-    "-secret", "--client_secret", metavar="client_secret",
-    required=True, help= "The client ID of your app"
-)
-
-# Agent name for reddit 
-parser.add_argument(
-    "-agent", "--user_agent", metavar="user_agent",
-    required=True, help= "The client ID of your app"
-)
-
-# Format of exporting
-parser.add_argument(
-    "-data", "--datatype", metavar="datatype",
-    required=False, default="json",help= "Return as CSV or json"
-)
-
-
-args = parser.parse_args()
-print(args.datatype)
-#    client_id="dX5FicqjbH7MM2nwlHnL3Q",
-#    client_secret="lgzyJMsDBpQDsnHeW0Z-FNiEWQA5Cg",
-#    user_agent="cute-subreddit-scrapper"
 reddit = praw.Reddit(
     client_id=args.client_id,
     client_secret=args.client_secret,
     user_agent=args.user_agent
 )
+print(type(reddit))
 
 # Setting up for gathering info from post
 submission_list = []
-for submission in reddit.subreddit("all").top(time_filter="week", limit=10):
+subreddits = subreddits_parser(args, reddit)
+
+for submission in subreddits:
     temp_dict = {
         "subreddit": submission.subreddit.display_name,
         "id": submission.id,
@@ -57,6 +29,8 @@ for submission in reddit.subreddit("all").top(time_filter="week", limit=10):
         "url": submission.url
     }
     submission_list.append(temp_dict)
-    
-export_json(submission_list)
-export_csv(submission_list)
+
+if args.datatype == "json":    
+    export_json(submission_list)
+else:
+    export_csv(submission_list)
--- src/reddit_scrapper/models/command_line/command_line.py
+++ src/reddit_scrapper/models/command_line/command_line.py
@@ -1,60 +1,70 @@
 import argparse
+import praw
 
-class CommandLine():
+def parser():
+    parser = argparse.ArgumentParser(
+        description="Provide databack as json or sql"
+    )
+
+    # Client ID from reddit app
+    parser.add_argument(
+        "-id", "--client_id", metavar="client_id",
+        required=True, help= "The client ID of your app"
+    )
+
+    # Client secret from reddit app
+    parser.add_argument(
+        "-secret", "--client_secret", metavar="client_secret",
+        required=True, help= "The client ID of your app"
+    )
+
+    # Agent name for reddit 
+    parser.add_argument(
+        "-agent", "--user_agent", metavar="user_agent",
+        required=True, help= "The client ID of your app"
+    )
+
+    # Subbreddit to scrape from
+    parser.add_argument(
+        "-sub", "--subreddit", metavar="subreddit",
+        required=False, default="all",help= "Subreddit to scrape from"
+    )
+
+    # Sort by order 
+    parser.add_argument(
+        "-sort", "--sort_by", metavar="sort_by", choices=['hot', 'new', 'top', 'rising', 'controversial'],
+        required=False, default="hot",help= "Sort by order of subreddit, default is hot (hot, new, top, rising, controversial)"
+    )
+
+    # Time frame (only need for top and controversial sort oders)
+    parser.add_argument(
+        "-frame", "--time_frame", metavar="time_frame",
+        required=False, default="month",
+        help= 'Time frame, only need for top and controversial sort oders, default is month ("all", "day", "hour", "month", "week", or "year")'
+    )
+
+    # Number of post
+    parser.add_argument(
+        "-limit", "--limit", metavar="limit", type=int,
+        required=False, default=100,help= "The number of posts returns, the default is 100, anything over 100 could be unstable"
+    )
+
+    # Format of exporting
+    parser.add_argument(
+        "-data", "--datatype", metavar="datatype",
+        required=False, default="json",help= "Return as CSV or json"
+    )
     
-parser = argparse.ArgumentParser(
-    description="Provide databack as json or sql"
-)
+    return parser.parse_args()
 
-# Client ID from reddit app
-parser.add_argument(
-    "-id", "--client_id", metavar="client_id",
-    required=True, help= "The client ID of your app"
-)
-
-# Client secret from reddit app
-parser.add_argument(
-    "-secret", "--client_secret", metavar="client_secret",
-    required=True, help= "The client ID of your app"
-)
-
-# Agent name for reddit 
-parser.add_argument(
-    "-agent", "--user_agent", metavar="user_agent",
-    required=True, help= "The client ID of your app"
-)
-
-# Subbreddit to scrape from
-parser.add_argument(
-    "-sub", "--subreddit", metavar="subreddit",
-    required=False, default="all",help= "Subreddit to scrape from"
-)
-
-# Sort by order 
-parser.add_argument(
-    "-sort", "--sort_by", metavar="sort_by",
-    required=False, default="hot",help= "Sort by order of subreddit (hot, new, top, rising, controversial)"
-)
-
-# Time frame (only need for top and controversial sort oders)
-parser.add_argument(
-    "-frame", "--time_frame", metavar="time_frame",
-    required=False, default="month",
-    help= 'Time frame, only need for top and controversial sort oders ("all", "day", "hour", "month", "week", or "year")'
-)
-
-# Number of post
-parser.add_argument(
-    "-limit", "--limit", metavar="limit",
-    required=False, default=100,help= "The number of posts returns, the default is 100, anything over 100 could be unstable"
-)
-
-# Format of exporting
-parser.add_argument(
-    "-data", "--datatype", metavar="datatype",
-    required=False, default="json",help= "Return as CSV or json"
-)
-
-args = parser.parse_args()
-s = "Hello {args.datatype}"
-print(s)
+def subreddits_parser(args: argparse.Namespace, reddit: praw.reddit.Reddit):
+    if args.sort_by == "hot":
+        return reddit.subreddit(args.subreddit).hot(limit=args.limit)
+    elif args.sort_by == "new":
+        return reddit.subreddit(args.subreddit).new(limit=args.limit)
+    elif args.sort_by == "rising":
+        return reddit.subreddit(args.subreddit).new(limit=args.limit)
+    elif args.sort_by == "top":
+        return reddit.subreddit(args.subreddit).top(time_filter=args.time_frame,limit=args.limit)
+    else:
+        return reddit.subreddit(args.subreddit).controversial(time_filter=args.time_frame,limit=args.limit)
